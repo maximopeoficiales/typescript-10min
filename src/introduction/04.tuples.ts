88192a91b9cb2adcc66f3428c36f4b2f42f50b56
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-09-17 08:19:05 -0500

    add: caso de uso para symbols

--- src/introduction/04.tuples.ts
+++ src/introduction/04.tuples.ts
@@ -1,8 +1,12 @@
-// tuplas
+// tuplas en js
+
+// https://www.youtube.com/watch?v=vImkxi6wCN0
 
 // Un valor inmutable es aquel cuyo contenido no se puede cambiar sin crear un valor completamente nuevo . En JavaScript, los valores primitivos son inmutables: una vez que se crea un valor primitivo, no se puede cambiar, aunque se puede reasignar otro valor a la variable que lo contiene.
+
 // Declare a tuple type
 let x: [string, number] = ["hello", 10]; // OK
+let x1: [string, number] = ["hello", 10]; // OK
 // Initialize it incorrectly
 x = [10, "hello"]; // Error
 x[0]=1;
@@ -11,4 +15,4 @@ console.log(x[0].substring(1));
 // ERROR
 console.log(x[1].substring(1));
 
-console.log(x);
+// console.log(x===x1);
--- src/introduction/13.less-common-primitives.ts
+++ src/introduction/13.less-common-primitives.ts
@@ -2,15 +2,43 @@
 // Creating a bigint via the BigInt function
 const oneHundred: bigint = BigInt(100);
 
+/* El BigInt tipo de datos tiene como objetivo permitir que los programadores de JavaScript representen valores enteros más grandes que el rango admitido por el Numbertipo de datos. La capacidad de representar números enteros con precisión arbitraria es especialmente importante cuando se realizan operaciones matemáticas con números enteros grandes. Con BigInt, el desbordamiento de enteros ya no será un problema. */
+
+
 // Creating a BigInt via the literal syntax
 const anotherHundred: bigint = 100n;
 
-
+// El tipo Symbol nos permite obtener valores que no pueden volver a ser creados, es decir, son identificadores únicos e inmutables.
 
 const firstName = Symbol("name");
 const secondName = Symbol("name");
 
-if (firstName === secondName) {
-    // This condition will always return 'false' since the types 'typeof firstName' and 'typeof secondName' have no overlap.
-    // Can't ever happen
+// if (firstName === secondName) {
+
+// }
+
+
+const createObject = () => {
+  let obj: any = {};
+  let id = Symbol("identificador");
+  obj[id] = 0;
+  return {
+    setId(valor: any) {
+      obj[id] = valor;
+    },
+    getId() {
+      return obj[id];
+    },
+    get() {
+      return obj;
+    }
+  }
 }
+let newObject = createObject();
+let object = newObject.get();
+console.log(object);
+newObject.setId(32);
+let t = newObject.getId();
+console.log(t);
+console.log(object);
+
